9bb7889634a0f8d13bc3dd9f53055bddbfb73b6f
Subhanshi <9528chotu@gmail.com>
2024-12-08 00:15:54 +0530

    code push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <ul>
+            <li className='font-bold text-teal-500 text-[50px]'>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
